fade66141a81a3b3a3e9f45818c08d859d48c53d
Riosmena <A01704245@tec.mx>
2023-03-07 13:51:37 -0600

    Spiderman y Ironman

---
+++ DIBUJOS_SKETCHPY/Spiderman.py
@@ -0,0 +1,10 @@
+"""
+José Emiliano Riosmena Castañón - A01704245
+Para configurar:
+1. Abrir la terminal
+2. Escribir: pip install sketchpy
+"""
+
+from sketchpy import library as lib
+obj = lib.tom_holland()
+obj.draw()
